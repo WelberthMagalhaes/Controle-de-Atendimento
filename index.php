2ff0ee37f660f6111c5af7f658bfdcc3f5713665
WelberthMagalhaes <welberthheider@gmail.com>
2022-10-04 15:52:10 -0300

    Menu inicial criado

--- index.php
+++ index.php
@@ -1,28 +1,40 @@
 <?php
-include_once("connect.php");
+#include_once("connect.php");
+
+
+
+
+$action = (isset($_GET['action'])) ? $_GET['action'] : NULL;
 
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br" dir = "ltr">
+<html lang="pt-br" dir="ltr">
+
 <head>
     <meta charset="UTF-8">
     <title>HOME</title>
 </head>
+
 <body>
-    
-    <div style = "display: flex; flex-direction: row; justify-content: center; align-items: center">
+
+    <div style="display: flex; flex-direction: row; justify-content: center; align-items: center">
         <h2> ATENDIMENTO </h2>
     </div>
-    <div style = "display: flex; flex-direction: row; justify-content: center; align-items: center">
-        <nav>#TODO como enviar valor através de href ou outro
-            <a style = "margin-right: 30px" href="index.php" value="add">Incluir</a>            
+    <div style="display: flex; flex-direction: row; justify-content: center; align-items: center">
+        <nav>
+            <a style="margin-right: 30px" href="index.php">Home</a>
             
-            <a style = "margin-right: 30px" href="index.php" value="edit">Alterar</a>
-            
-            <a href="index.php" value="delete">Deletar</a>
+            <a style="margin-right: 30px" href="index.php?action=add">Incluir</a>
+
+            <a style="margin-right: 30px" href="index.php?action=edit">Alterar</a>
+
+            <a href="index.php?action=delete">Deletar</a>
         </nav>
     </div>
-    
+
+    <!--TODO Criar SwitchCase-->
+
 </body>
+
 </html>
