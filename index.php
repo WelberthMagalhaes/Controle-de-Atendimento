3bb667d2fd7e6ded667557421a13d9a830645952
WelberthMagalhaes <welberthheider@gmail.com>
2022-10-03 23:06:08 -0300

    Pagina Home iniciada

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include_once("connect.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br" dir = "ltr">
+<head>
+    <meta charset="UTF-8">
+    <title>HOME</title>
+</head>
+<body>
+    
+    <div style = "display: flex; flex-direction: row; justify-content: center; align-items: center">
+        <h2> ATENDIMENTO </h2>
+    </div>
+    <div style = "display: flex; flex-direction: row; justify-content: center; align-items: center">
+        <nav>#TODO como enviar valor através de href ou outro
+            <a style = "margin-right: 30px" href="index.php" value="add">Incluir</a>            
+            
+            <a style = "margin-right: 30px" href="index.php" value="edit">Alterar</a>
+            
+            <a href="index.php" value="delete">Deletar</a>
+        </nav>
+    </div>
+    
+</body>
+</html>
